b9196d8d5742421e1d63e249f4e8ca3c34ad6ebb
cdw0223 <caodawei@caodaweideMacBook-Pro.local>
2014-09-02 11:09:35 +0800

    up

--- private_share/SettingViewController.m
+++ private_share/SettingViewController.m
@@ -43,68 +43,56 @@
     [backButton addTarget:self action:@selector(dismissViewController) forControlEvents:UIControlEventTouchUpInside];
     [backButton setImage:[UIImage imageNamed:@"new_back"] forState:UIControlStateNormal];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
-
-    UIImageView *perfectinformation1 = [[UIImageView alloc]initWithFrame:CGRectMake(20, 22, 345/2, 35/2)];
-    perfectinformation1.image = [UIImage imageNamed:@"perfectinformation1"];
-    [self.view addSubview:perfectinformation1];
     
-     perfectinformation2 = [[UIImageView alloc]initWithFrame:CGRectMake(30+perfectinformation1.bounds.size.width, 32, 200/2, 162/2)];
-    perfectinformation2.image = [UIImage imageNamed:@"perfectinformation2"];
-    [self.navigationController.navigationBar addSubview:perfectinformation2];
-
-    UIImage *bgImage = [UIImage imageNamed:@"setupbg"];
-    UIEdgeInsets insets = UIEdgeInsetsMake(10, 10, 10, 10);
-    bgImage = [bgImage resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch];
-    
-    UIImageView *bgImageView = [[UIImageView alloc]initWithFrame:CGRectMake(15, 81-12, self.view.bounds.size.width-30, [UIDevice is4InchDevice] ? 330 :250)];
-    bgImageView.image = bgImage;
-    [self.view addSubview:bgImageView];
-    
-    tableview = [[UITableView alloc]initWithFrame:CGRectMake(35, bgImageView.frame.origin.y+2, bgImageView.bounds.size.width-40, [UIDevice is4InchDevice] ? 326 :246) style:UITableViewStyleGrouped];
-    tableview.scrollEnabled = NO;
+    tableview = [[UITableView alloc]initWithFrame:CGRectMake(0, 0,self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStyleGrouped];
+//    tableview.scrollEnabled = NO;
+    tableview.backgroundColor = [UIColor clearColor];
     tableview.delegate = self;
     tableview.dataSource = self;
     tableview.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:tableview];
 
     
-    UIButton *editBtn = [[UIButton alloc]initWithFrame:CGRectMake(220, bgImageView.frame.origin.y + bgImageView.bounds.size.height, 143/2, 91/2)];
+    UIView *topView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, tableview.bounds.size.width, 81)];
+    topView.backgroundColor = [UIColor clearColor];
+    tableview.tableHeaderView = topView;
+
+    UIImageView *perfectinformation1 = [[UIImageView alloc]initWithFrame:CGRectMake(20, 34, 345/2, 35/2)];
+    perfectinformation1.image = [UIImage imageNamed:@"perfectinformation1"];
+    [topView addSubview:perfectinformation1];
+    
+     perfectinformation2 = [[UIImageView alloc]initWithFrame:CGRectMake(30+perfectinformation1.bounds.size.width, 0, 200/2, 162/2)];
+    perfectinformation2.image = [UIImage imageNamed:@"perfectinformation2"];
+    [topView addSubview:perfectinformation2];
+
+//    UIImage *bgImage = [UIImage imageNamed:@"setupbg"];
+//    UIEdgeInsets insets = UIEdgeInsetsMake(10, 10, 10, 10);
+//    bgImage = [bgImage resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch];
+//    
+//    UIImageView *bgImageView = [[UIImageView alloc]initWithFrame:CGRectMake(15, 81-12, self.view.bounds.size.width-30, [UIDevice is4InchDevice] ? 330 :250)];
+//    bgImageView.image = bgImage;
+//    [self.view addSubview:bgImageView];
+//    
+//
+//
+    UIView *bottomView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, tableview.bounds.size.width, 100)];
+    bottomView.backgroundColor = [UIColor clearColor];
+    tableview.tableFooterView = bottomView;
+    
+    UIButton *editBtn = [[UIButton alloc]initWithFrame:CGRectMake(220, -15, 143/2, 91/2)];
     [editBtn setBackgroundImage:[UIImage imageNamed:@"edit"] forState:UIControlStateNormal];
     [editBtn addTarget:self action:@selector(editBtnClick) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:editBtn];
+    [bottomView addSubview:editBtn];
     
-    UIButton *exitBtn =[[UIButton alloc]initWithFrame:CGRectMake(20, editBtn.frame.origin.y + editBtn.bounds.size.height, self.view.bounds.size.width-40, 40)];
+    UIButton *exitBtn =[[UIButton alloc]initWithFrame:CGRectMake(20, editBtn.bounds.size.height-15, tableview.bounds.size.width-40, 40)];
     [exitBtn setTitle:NSLocalizedString(@"logout", @"") forState:UIControlStateNormal];
     exitBtn.titleLabel.font = [UIFont systemFontOfSize:15.f];
     [exitBtn setTintColor:[UIColor whiteColor]];
     [exitBtn setBackgroundImage:[UIImage imageNamed:@"button"] forState:UIControlStateNormal];
     [exitBtn setTitleEdgeInsets:UIEdgeInsetsMake(7, 0, 0, 0)];
     [exitBtn addTarget:self action:@selector(exitBtnClick) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:exitBtn];
+    [bottomView addSubview:exitBtn];
 
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
--(void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    [perfectinformation2 setHidden:NO];
-}
-
--(void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-    [perfectinformation2 setHidden:YES];
-
-}
-
--(void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
 }
 
 -(void)editBtnClick
