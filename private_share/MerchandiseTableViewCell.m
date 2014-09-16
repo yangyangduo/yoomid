8ef296d0c9c3108ad411c85f45a4b93431496694
cdw0223 <caodawei@caodaweideMacBook-Pro.local>
2014-09-16 11:46:36 +0800

    up

--- private_share/MerchandiseTableViewCell.m
+++ private_share/MerchandiseTableViewCell.m
@@ -32,6 +32,7 @@
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+    
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         self.selectionStyle = UITableViewCellSelectionStyleNone;
